d6b34c672b1da53691305881ec63d6fd60faf998
Neil <n.delargy@ctidigital.com>
2016-12-11 21:44:48 +0000

    adds some update and delete playground code

---
+++ playground/delete.js
@@ -0,0 +1,26 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('unable to connect to mongodb');
+    console.log(err);
+    return;
+  }
+  //
+  // db.collection('Todos').find({"_id": ObjectID("5847c3dba1a787aab9726ca3")}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todos");
+  //   console.log(JSON.stringify(err, undefined, 2));
+  // });
+
+
+    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+      console.log(result);
+    }, (err) => {
+      console.log("Unable to delete todos");
+      console.log(JSON.stringify(err, undefined, 2));
+    });
+
+  // db.close();
+});
