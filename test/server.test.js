14a9e459015c56e93b01a9874eea02cda0ce26e7
Neil <n.delargy@ctidigital.com>
2016-12-14 09:39:16 +0000

    adds test for get todos

--- test/server.test.js
+++ test/server.test.js
@@ -4,7 +4,20 @@ const request = require('supertest');
 const {app} = require('../server/server.js');
 const {Todo} = require('../server/model/todo.js');
 
+const todos = [
+  {
+    "text": "spank your monkey",
+  },
+  {
+    "text": "spank my monkey",
+  },
+  {
+    "text": "spank the monkey",
+  }
+];
+
 beforeEach((done) => {
+  Todo.insertMany(todos);
   Todo.remove({}).then(() => done());
 });
 
@@ -21,7 +34,7 @@ describe('POST todos', () => {
        if (err) {
          return done(err);
        }
-       Todo.find().then((todos) => {
+       Todo.find({text}).then((todos) => {
          expect(todos.length).toBe(1);
          expect(todos[0].text).toBe(text);
          done();
@@ -38,9 +51,21 @@ describe('POST todos', () => {
         return done(err);
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(3);
         done();
-      }).catch((e) => done());
+      }).catch((e) => done(e));
     });
   })
+});
+
+describe('GET todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos')
+    .send()
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(3)
+    })
+    .end(done);
+  });
 });
