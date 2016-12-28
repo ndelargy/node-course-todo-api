fceb22ed0db7005da852ad92b5e3348b600baa01
Neil Delargy <neil.delargy@tribalworldwide.co.uk>
2016-12-28 22:41:21 +0000

    sorts out the mistakenly committed line

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 
-ds133378.mlab.com:33378/node-course-todos -u chim -p freaklechic
-
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodosApp');
 
