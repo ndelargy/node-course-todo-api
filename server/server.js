1caed8da3db2ce5f9e8c14fb34d6326eefb9d8d6
Neil Delargy <neil.delargy@tribalworldwide.co.uk>
2017-01-04 18:56:43 +0000

    adds patch update route /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var {ObjectID} = require('mongodb');
 
@@ -61,6 +62,28 @@ app.delete('/todos/:id', (req, res) => {
     }
   }).catch((e) => {
     return res.status(400);
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();;
+  }
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+    body.completed = false;
+  }
+
+  Todo.findByIdAndUpdate(req.params.id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
   });
 });
 
